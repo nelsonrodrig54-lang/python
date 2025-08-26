10eb0b7b550aa488c589ebe45e64a86143da6c60
nelsonrodrig54-lang <nelson.rodrig54@gmail.com>
2025-08-26 11:27:17 -0500

    Update joke.py

--- joke.py
+++ joke.py
@@ -1,1 +1,16 @@
+"""
+Create an original knock-knock joke or find one you 
+like online and use it when you write your program.
+ Submit the .py file. Grading is complete/incomplete, 
+ and you may resubmit within a week of when the 
+ submission is graded. Include at least one comment 
+ (starts with # ) to describe the program's purpose.
 
+"""
+
+#knock-knock your socks off
+print("knock,knock!")
+print("Who's there?")
+print("Venice.")
+print("Venice who?")
+print("Venice your dad coming home?")
